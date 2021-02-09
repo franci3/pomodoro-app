10d08fad5365f5bbb83e1f62187608c9ed0d1792
Franco Vela <gianfranco.vela@hsrw.org>
2021-02-10 00:28:59 +0100

    refactor screens for maintainability

---
+++ lib/screens/card_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pomodoro_app/assets/custom_theme.dart';
+import 'package:pomodoro_app/states/home_controller.dart';
+
+class CardScreen extends StatelessWidget {
+  final HomeController homeController = Get.put(HomeController());
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.zero,
+      color: PomodoroValues.cardColor,
+      elevation: 15,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20))),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 70.0),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Obx(() => SizedBox(
+                    width: 250,
+                    height: 250,
+                    child: CircularProgressIndicator(
+                        value: _calculateCountdownLoader(homeController),
+                        backgroundColor: Colors.white12,
+                        strokeWidth: 18,
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                            PomodoroValues.mainColor)),
+                  )),
+              _countdownWidget(homeController)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  double _calculateCountdownLoader(HomeController homeController) {
+    return 1 - (homeController.totalSeconds.value / 1500);
+  }
+}
+
+Widget _countdownWidget(HomeController homeController) {
+  return Obx(() => Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      homeController.countdownText(),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          homeController.timerIsActive.isTrue
+              ? IconButton(
+            iconSize: 40,
+            color: PomodoroValues.mainColor,
+            onPressed: () => homeController.pauseTimer(),
+            icon: Icon(homeController.timerIsPaused.isTrue
+                ? Icons.play_arrow
+                : Icons.pause),
+          )
+              : Container(),
+          IconButton(
+              iconSize: 40,
+              color: PomodoroValues.mainColor,
+              onPressed: () => homeController.startTimer(),
+              icon: Icon(homeController.timerIsActive.isTrue
+                  ? Icons.stop
+                  : Icons.play_arrow)),
+        ],
+      )
+    ],
+  ));
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pomodoro_app/assets/custom_theme.dart';
+import 'package:pomodoro_app/screens/card_screen.dart';
 import 'package:pomodoro_app/screens/statistics_screen.dart';
 import 'package:pomodoro_app/states/home_controller.dart';
 
 class PomodoroHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final HomeController homeController = Get.put(HomeController());
     return Scaffold(
       appBar: AppBar(
         backgroundColor: PomodoroValues.cardColor,
@@ -23,75 +23,10 @@ class PomodoroHome extends StatelessWidget {
         shrinkWrap: true,
         physics: BouncingScrollPhysics(),
         children: [
-          Card(
-              margin: EdgeInsets.zero,
-              color: PomodoroValues.cardColor,
-              elevation: 15,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20))),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 70.0),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Obx(() => SizedBox(
-                            width: 250,
-                            height: 250,
-                            child: CircularProgressIndicator(
-                                value:
-                                    _calculateCountdownLoader(homeController),
-                                backgroundColor: Colors.white12,
-                                strokeWidth: 18,
-                                valueColor: AlwaysStoppedAnimation<Color>(
-                                    PomodoroValues.mainColor)),
-                          )),
-                      _countdownWidget(homeController)
-                    ],
-                  ),
-                ),
-              )),
+          CardScreen(),
           StatisticsScreen()
         ],
       ),
     );
   }
-
-  double _calculateCountdownLoader(HomeController homeController) {
-    return 1 - (homeController.totalSeconds.value / 1500);
-  }
-}
-
-Widget _countdownWidget(HomeController homeController) {
-  return Obx(() => Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          homeController.countdownText(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              homeController.timerIsActive.isTrue
-                  ? IconButton(
-                      iconSize: 40,
-                      color: PomodoroValues.mainColor,
-                      onPressed: () => homeController.pauseTimer(),
-                      icon: Icon(homeController.timerIsPaused.isTrue
-                          ? Icons.play_arrow
-                          : Icons.pause),
-                    )
-                  : Container(),
-              IconButton(
-                  iconSize: 40,
-                  color: PomodoroValues.mainColor,
-                  onPressed: () => homeController.startTimer(),
-                  icon: Icon(homeController.timerIsActive.isTrue
-                      ? Icons.stop
-                      : Icons.play_arrow)),
-            ],
-          )
-        ],
-      ));
 }
