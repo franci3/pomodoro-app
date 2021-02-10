ad04f90ae31d71d38f08918c3291cff16459f656
Vela, Gianfranco <gianfranco.vela@hsrw.org>
2021-02-10 19:28:26 +0000

    First Pub

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pomodoro_app
+# Pomodoro Timer
 
 A Pomodoro mobile application.
 
@@ -7,3 +7,9 @@ A Pomodoro mobile application.
 This is an open source Pomodoro Timer for focused work. It has a set 25 minutes timer with 5 minutes
 pause every round. After 4 rounds there is a longer 15 minutes pause.
 
+## Motivation
+
+Die App dient zum einen als Lernhilfe für den Nutzer, zum anderen als eine Spielwiese für mich, dass
+neue StateManagement Framework GetX für Flutter auszuprobieren. Bisher liebe ich es.
+
+    
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:pomodoro_app/assets/custom_theme.dart';
@@ -6,6 +7,8 @@ import 'package:pomodoro_app/screens/home.dart';
 import 'package:pomodoro_app/states/statistics_controller.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   Paint.enableDithering = true;
   await GetStorage.init();
   Get.put(StatisticsController());
